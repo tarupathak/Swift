d07b829a25446a347703a91eca50f6261e81421b
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-09-24 15:25:33 +0530

    Added case studies and why swift section

--- src/Pages/WhySwift.jsx
+++ src/Pages/WhySwift.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import bgImg from "../Assets/bgImg.svg";
 import "./Pages.css";
+import swissImg from "../Assets/whySwissImg.svg";
+import coatsHire from "../Assets/coatsHire.svg";
+import system from "../Assets/system.svg";
+import abc from "../Assets/abc.svg";
 
 const WhySwift = () => {
   return (
@@ -43,6 +47,43 @@ const WhySwift = () => {
           <div className="homeBox">
             <div className="redBox"></div>
             <div className="redBox"></div>
+          </div>
+        </div>
+      </div>
+      {/* <-----------------Why swift-----------------> */}
+      <div className="whySwift">
+        <div className="whiteBox">
+          <h2>Why Swift</h2>
+          <p>
+            As a supplier we are a responsive business partner that can ensure
+            quality and supply. We value strong relationships with our customers
+            and believe that commercial relationships are a learnt skill. Our
+            integrated supply chain helps us to ensure a high standard of
+            product quality and service delivery. This reduces the potential
+            need for rework, increases reliability of service and saves our
+            customers time and money. Swift Transport is a market leader
+            providing flexible, client-focused on demand transport and 3PL
+            services complimented by industry leading technology. We offer
+            solutions that simultaneously improve efficiencies, consistency,
+            reliability and bottom line savings for our clients' businesses.
+          </p>
+        </div>
+        <div className="swissImg">
+          <img src={swissImg} alt="swissImg" />
+        </div>
+      </div>
+      {/* <-----------------Case Studies-----------------> */}
+      <div className="caseStudy">
+        <h2>Case Studies</h2>
+        <div className="whiteBg">
+          <div className="boxImg">
+            <img src={coatsHire} />
+          </div>
+          <div className="boxImg">
+            <img src={abc} />
+          </div>
+          <div className="boxImg">
+            <img src={system} />
           </div>
         </div>
       </div>
